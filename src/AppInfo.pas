9770d86bfdf368435100d54fe49c0552a07c45d7
Juan Manuel Soltero Sánchez <9614449@alu365.murciaeduca.es>
2023-05-02 19:03:04 +0200

    Update número de versión

--- src/AppInfo.pas
+++ src/AppInfo.pas
@@ -2,7 +2,7 @@
  * @Author: Juan Manuel Soltero Sánchez
  * @Date:   2023-04-06 14:37:57
  * @Last Modified by:   Juan Manuel Soltero Sánchez
- * @Last Modified time: 2023-04-24 19:38:57
+ * @Last Modified time: 2023-05-02 19:01:50
  *)
 {
 
@@ -41,8 +41,8 @@ uses
 
 // Inicio de programación  20230406
 const
-  VERSION_PROGRAMA     = '1.0.7.5' + {$IFDEF CPUX64}' x64'{$ELSE}' x86'{$ENDIF};
-  FECHA_PROGRAMA       = '20230424 19:40:41';
+  VERSION_PROGRAMA     = '1.1.0.6' + {$IFDEF CPUX64}' x64'{$ELSE}' x86'{$ENDIF};
+  FECHA_PROGRAMA       = '20230502 19:03:41';
   NOMBRE_PROGRAMA      = 'CaMeEx';
   NOMBRE_AUTOR         = 'Juan Manuel Soltero Sánchez';
   APP_WEB              = 'https://github.com/juanmasolsan/CaMeEx';
