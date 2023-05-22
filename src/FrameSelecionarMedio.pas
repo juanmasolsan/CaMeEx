21a1fb187194f03e902616694937afdda5988d4a
Juan Manuel Soltero Sánchez <9614449@alu365.murciaeduca.es>
2023-05-22 22:55:06 +0200

    Agregado al Frame de selección de medio y datos básicos : Cambio de imagen al cambiar de dispositivo y añadido imagen a cada tipo de dispositivo

--- src/FrameSelecionarMedio.pas
+++ src/FrameSelecionarMedio.pas
@@ -2,7 +2,7 @@
  * @Author: Juan Manuel Soltero Sánchez
  * @Date:   2023-05-21 22:40:01
  * @Last Modified by:   Juan Manuel Soltero Sánchez
- * @Last Modified time: 2023-05-22 22:29:19
+ * @Last Modified time: 2023-05-22 22:53:11
  *)
 unit FrameSelecionarMedio;
 
@@ -13,7 +13,14 @@ interface
 uses
    Windows, Classes, SysUtils, Forms, Controls, ExtCtrls, StdCtrls, SynEdit, FGL, ItemBaseDatos, Types;
 
+
+
 type
+  TUnidadDetectada = class
+    Ruta : string;
+    Tipo : TItemDatoTipo;
+  end;
+
   { TFrame_SelecionarMedio }
   TFrame_SelecionarMedio = class(TFrame)
     ButtonSeleccionarRuta: TButton;
@@ -25,7 +32,7 @@ type
     Edit_Nombre_error1: TLabel;
     Edit_Nombre_error2: TLabel;
     Image1: TImage;
-    Image2: TImage;
+    ImageDispositivo: TImage;
     Label1: TLabel;
     Label11: TLabel;
     Label13: TLabel;
@@ -49,30 +56,28 @@ type
     procedure ComboBoxDispositivosDrawItem(Control: TWinControl; Index: Integer; ARect: TRect; State: TOwnerDrawState);
     procedure Edit_NombreChange(Sender: TObject);
   private
-    protected
-      procedure DoGenerarUnidades();
+  protected
+    // Construye las la lista de unidades detectadas
+    procedure DoGenerarUnidades();
+
+    // Devuelve la unidad seleccionada
+    function GetUnidad(index : longint) : TUnidadDetectada;
   public
+    // Constructor y destructor
     constructor Create(TheOwner: TComponent); override;
     destructor Destroy; override;
 
-
+    // Valida el formulario
     function IsValidate() : boolean;
   end;
 
 implementation
 
 uses
-   UnidadPrincipal,
- AppString;
+  UnidadPrincipal
+  , AppString;
 
 {$R *.lfm}
-
-
-type
-  TUnidadDetectada = class
-    Ruta : string;
-    Tipo : TItemDatoTipo;
-  end;
 
 
 { TFrame_SelecionarMedio }
@@ -81,6 +86,7 @@ constructor TFrame_SelecionarMedio.Create(TheOwner: TComponent);
 begin
   inherited Create(TheOwner);
 
+  // Genera la lista de unidades
   DoGenerarUnidades();
 end;
 
@@ -98,7 +104,7 @@ begin
   inherited Destroy;
 end;
 
-
+// Genera la lista de unidades
 procedure TFrame_SelecionarMedio.DoGenerarUnidades();
 
   procedure CrearUnidad(Nombre,  Ruta : string; Tipo: TItemDatoTipo);
@@ -117,6 +123,7 @@ procedure TFrame_SelecionarMedio.DoGenerarUnidades();
     OldMode: Word;
   begin
 
+    // Crea la unidad de Carpeta
     CrearUnidad(Message_Asistente_Nuevo_Catalogo_Tipo_Medio_Carpeta, '', TItemDatoTipo.RootCarpeta);
 
     // Empty Floppy or Zip drives can generate a Windows error.
@@ -152,19 +159,47 @@ begin
   IsValidate();
 end;
 
-procedure TFrame_SelecionarMedio.ComboBoxDispositivosChange(Sender: TObject);
+// Devuelve la unidad seleccionada
+function TFrame_SelecionarMedio.GetUnidad(index : longint) : TUnidadDetectada;
 begin
+  Result := nil;
+  if index > -1 then
+    Result := TUnidadDetectada(ComboBoxDispositivos.Items.Objects[index]);
+end;
+
+procedure TFrame_SelecionarMedio.ComboBoxDispositivosChange(Sender: TObject);
+var
+  Index  : integer;
+  Unidad : TUnidadDetectada;
+begin
+  Unidad := GetUnidad(ComboBoxDispositivos.ItemIndex);
+  if Unidad <> nil then
+  begin
+    Index  := integer(Unidad .Tipo);
+
+    // Cambia la imagen del dispositivo
+    Form_Principal.ImageListArchivos32.GetBitmap(Index, ImageDispositivo.Picture.Bitmap);
+  end;
+
   //TODO: Al seleccionar un dispositivo que muestre la ruta actual, si no es carpeta, que la ruta esté bloqueada
 end;
 
 procedure TFrame_SelecionarMedio.ComboBoxDispositivosDrawItem(Control: TWinControl; Index: Integer; ARect: TRect; State: TOwnerDrawState);
 var
   indexImage : longint;
+
+  Unidad : TUnidadDetectada;
 begin
+  // Dibuja el texto y el fondo
   ComboBoxDispositivos.Canvas.FillRect(ARect);                                                    //first paint normal background
   ComboBoxDispositivos.Canvas.TextRect(ARect, 22, ARect.Top, ComboBoxDispositivos.Items[Index]);  //paint item text
 
   indexImage := integer(TItemDatoTipo.Root);
+  Unidad := GetUnidad(Index);
+  if Unidad <> nil then
+    indexImage := integer(Unidad .Tipo);
+
+  // Dibuja la imagen
   Form_Principal.ImageListArchivos.Draw(ComboBoxDispositivos.Canvas, ARect.Left + 1, ARect.Top + 1, indexImage);  //draw image according to index on canvas
 end;
 
@@ -176,12 +211,10 @@ begin
   Edit_Nombre_error.Visible := false;
 
   if Edit_Nombre.Text = '' then
-   begin
-     Result := false;
-     Edit_Nombre_error.Visible := true;
-   end;
-
-
+  begin
+    Result := false;
+    Edit_Nombre_error.Visible := true;
+  end;
 end;
 
 end.
